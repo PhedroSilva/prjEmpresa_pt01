5cd6c836dbe9a051252a1ad9732b7ba9267457b1
PhedroSilva <133990374+PhedroSilva@users.noreply.github.com>
2023-11-23 15:51:57 -0300

    Add files via upload

---
+++ prjempresa/src/main/java/com/senai/prjempresa/PrjempresaApplication.java
@@ -0,0 +1,13 @@
+package com.senai.prjempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjempresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjempresaApplication.class, args);
+	}
+
+}
